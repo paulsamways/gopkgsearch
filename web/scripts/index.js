1ed54e0d749b0aa77d91b784b7e3a1d3c9a23e70
Paul Samways <paul@paulsamways.com>
2012-04-17 20:33:28 +1000

    implemented keyboard bindings for r navigating results

--- web/scripts/index.js
+++ web/scripts/index.js
@@ -1,4 +1,10 @@
 $(function() {
+  var _KEYUP = 38;
+  var _KEYDOWN = 40;
+  var _KEYESC = 27;
+  var _KEYENTER = 13;
+  var _KEYSPACE = 32;
+
   var submitTimer = null;
   
   var $results = $("#results");
@@ -23,6 +29,57 @@ $(function() {
             "</li>";
   };
 
+  var deselect = function() {
+    $("li.selected", $results).removeClass("selected");
+  };
+  var select = function($li) {
+    if ($li === undefined) {
+      $li = $("li:first", $results);
+    }
+
+    $li.addClass("selected");
+
+    if ($li.length > 0) {
+      $li[0].scrollIntoView(false);
+    }
+  };
+  var selectNext = function() {
+    var $s = $("li.selected", $results).removeClass("selected");
+    
+    var $n = $s.next("li");
+
+    if ($n.length === 0) {
+      select();
+    } else {
+      select($n);
+    }
+  };
+  var selectPrev = function() {
+    var $s = $("li.selected", $results).removeClass("selected");
+    var $p = $s.prev();
+
+    if ($p.length === 0) {
+      $p = $("li:last", $results);
+    }
+
+    select($p);
+  };
+
+  var reset = function() {
+    deselect();
+    $("li", $results).remove();
+    $input.val("");
+    $input.focus();
+  };
+
+  var expand = function($li) {
+    if ($li === undefined) {
+      $li = $("li.selected", $results);
+    }
+
+    $("div.doc, pre.source", $li).toggle();
+  };
+
   var submitQuery = function(q) {
    
     var callback = function(data, status, xhr) {
@@ -36,10 +93,7 @@ $(function() {
           var $d = $(renderResult(r))
             .appendTo($results);
 
-            $d.click(function(e) {
-              $("div.doc, pre.source", this).toggle();
-            });
-
+          $d.click(function(e) { deselect(); expand($(this)); });
         }
       }
     };
@@ -55,13 +109,58 @@ $(function() {
   $input.keyup(function(e) {
     var self = this;
 
+    e.stopPropagation();
+
     if (submitTimer != null) {
       clearTimeout(submitTimer);
+    }
+
+    switch (e.which) {
+      case _KEYDOWN:
+        e.preventDefault();
+        $(self).blur();
+        select();
+        return;
+      case _KEYESC:
+        e.preventDefault();
+        reset();
+        return;
     }
 
     submitTimer = setTimeout(function() {
       submitQuery($(self).val());
     }, 50);
+  });
+  
+  $input.keypress(function(e) {
+    e.stopPropagation();
+  });
 
+
+  $(window).keyup(function(e) {
+    switch (e.which) {
+      case _KEYDOWN:
+        selectNext();
+        e.preventDefault();
+        return;
+      case _KEYUP:
+        selectPrev();
+        e.preventDefault();
+        return;
+      case _KEYESC:
+        reset();
+        e.preventDefault();
+        return;
+      case _KEYENTER:
+      case _KEYSPACE:
+        expand();
+        e.preventDefault();
+        return;
+    }
+  });
+  $(window).keydown(function(e) {
+    if (e.which === _KEYDOWN || e.which === _KEYUP) {
+      e.preventDefault();
+    }
   });
 });
